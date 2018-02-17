a1d0b3060c223379e22b34778b233d83b9cea956
Nicole Boettcher <11392575+deliciousnachos@users.noreply.github.com>
2018-02-17 15:03:36 -0500

    add starter files

---
+++ starter_code/part_1/movies-and-jurassic-park.js
@@ -0,0 +1,56 @@
+console.log('movies-and-jurassic-park.js is connected! woohoo!');
+
+// Part 1
+
+var snakewaterMontana = {
+  paleontologist: "Dr. Alan Grant",
+  depth: "10 meters",
+  specimen: "Velociraptor"
+};
+
+
+// Do the tasks down here!
+
+
+
+
+// Part 2
+
+var nicaragua = {
+  depth: "200 meters",
+  annual_budget: 1500000,
+  specimens: [
+    "Tyrannosaurus Rex",
+    "Stegosaurous",
+    "Triceratops",
+    "Velociraptor"
+  ],
+};
+
+// Do the tasks down here!
+
+
+
+// Part 3
+
+var hammondsMines = {
+  buenos_aires: {
+    depth: "400 meters",
+    annual_budget: 1000000,
+    specimens: [
+      "Dilophosaurus",
+      "Brachiosaurus"
+    ],
+  },
+  mexico: {
+    depth: "350 meters",
+    annual_budget: 900000,
+    specimens: [
+      "Gallimimus",
+      "Parasaurolophus"
+    ],
+  },
+};
+
+// Do the tasks down here!
+
