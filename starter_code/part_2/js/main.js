f80a539d4914d9485222ac7901a310173feb6936
Nicole Boettcher <11392575+deliciousnachos@users.noreply.github.com>
2018-02-18 12:00:47 -0500

    add conditions for background colors

--- starter_code/part_2/js/main.js
+++ starter_code/part_2/js/main.js
@@ -5,8 +5,26 @@
 document.querySelector('#convert').addEventListener('click', sendCels);
 
 function sendCels() {
+
   var celsInput = parseInt(document.querySelector('#celsius-input').value);
   var degF = celsInput * 9 / 5 +32;
-  document.querySelector('#degF').innerHTML = degF;
+  document.querySelector('#degF').innerHTML = degF + ' ºF';
+
+  if (degF > 60) {
+    document.querySelector('#fahrenheit-display').style['background-color'] = 'red';
+    document.querySelector('#fahrenheit-display').style['color'] = 'white';
+  }
+  else if (degF < 30) {
+    document.querySelector('#fahrenheit-display').style['background-color'] = 'blue';
+    document.querySelector('#fahrenheit-display').style.color = 'white';
+  }
+  else {
+    document.querySelector('#fahrenheit-display').style['background-color'] = 'lightgray';
+    document.querySelector('#fahrenheit-display').style.color = 'black';
+  };
+
+
+
+  
 }
 
