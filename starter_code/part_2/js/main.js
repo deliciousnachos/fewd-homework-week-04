f47952a56869b2a15e4dfe2e0882aad1a3cee824
Nicole Boettcher <11392575+deliciousnachos@users.noreply.github.com>
2018-02-18 12:45:21 -0500

    fixed formatting

--- starter_code/part_2/js/main.js
+++ starter_code/part_2/js/main.js
@@ -4,22 +4,18 @@ function sendCels() {
   var celsInput = parseInt(document.querySelector('#celsius-input').value);
   var degF = celsInput * 9 / 5 +32;
   document.querySelector('#degF').innerHTML = degF + ' ºF';
-
   if (degF > 60) {
     document.querySelector('#fahrenheit-display').style['background-color'] = 'red';
     document.querySelector('#fahrenheit-display').style['color'] = 'white';
-  }
-  else if (degF < 30) {
+  } else if (degF < 30) {
     document.querySelector('#fahrenheit-display').style['background-color'] = 'blue';
     document.querySelector('#fahrenheit-display').style.color = 'white';
-  }
-  else if (degF <=60 && degF >= 30) {
+  } else if (degF <=60 && degF >= 30) {
     document.querySelector('#fahrenheit-display').style['background-color'] = 'lightgray';
     document.querySelector('#fahrenheit-display').style.color = 'black';
-  }
-  else {
+  } else {
     document.querySelector('#fahrenheit-display').style['background-color'] = 'white';
     document.querySelector('#fahrenheit-display').style.color = 'black';
     document.querySelector('#degF').innerHTML = 'Invalid input, try again!';
   }
-}
+};
