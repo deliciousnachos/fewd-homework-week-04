baf3620bc04fb97a9a7ece6a09bd908359f0a433
Nicole Boettcher <11392575+deliciousnachos@users.noreply.github.com>
2018-02-18 02:32:44 -0500

    half of a convert to fahr function

--- starter_code/part_2/js/main.js
+++ starter_code/part_2/js/main.js
@@ -1,1 +1,18 @@
 // alert('Welcome to the Calculator');
+
+  document.querySelector('#convert').addEventListener('click', sendCels);
+
+function sendCels() {
+  var celsInput = parseInt(document.querySelector('#convert').value);
+  console.log(celsInput);
+}
+
+//
+//
+// function getAnswer() {
+//   var operand = document.querySelector('#operand').value;
+//   var firstnum = parseInt(document.querySelector('#firstnum').value);
+//   var lastnum = parseInt(document.querySelector('#lastnum').value);
+//   var result = calculator[operand](firstnum, lastnum);
+//   document.querySelector('h1').innerHTML = result;
+// }
