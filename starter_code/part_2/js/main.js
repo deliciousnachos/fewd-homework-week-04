954fb11b399891ec06652cc158bd8b609098f79c
Nicole Boettcher <11392575+deliciousnachos@users.noreply.github.com>
2018-02-18 11:29:06 -0500

    got the calculator to dang work

--- starter_code/part_2/js/main.js
+++ starter_code/part_2/js/main.js
@@ -1,18 +1,12 @@
 // alert('Welcome to the Calculator');
 
-  document.querySelector('#convert').addEventListener('click', sendCels);
+
+
+document.querySelector('#convert').addEventListener('click', sendCels);
 
 function sendCels() {
-  var celsInput = parseInt(document.querySelector('#convert').value);
-  console.log(celsInput);
+  var celsInput = parseInt(document.querySelector('#celsius-input').value);
+  var degF = celsInput * 9 / 5 +32;
+  document.querySelector('#degF').innerHTML = degF;
 }
 
-//
-//
-// function getAnswer() {
-//   var operand = document.querySelector('#operand').value;
-//   var firstnum = parseInt(document.querySelector('#firstnum').value);
-//   var lastnum = parseInt(document.querySelector('#lastnum').value);
-//   var result = calculator[operand](firstnum, lastnum);
-//   document.querySelector('h1').innerHTML = result;
-// }
